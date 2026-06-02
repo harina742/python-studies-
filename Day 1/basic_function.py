c9899defbb4a5198e539d6b32b608f4559ffe64c
HARINA.G <harinag@karunya.edu.in>
2026-06-02 21:05:24 +0530

    Create basic_function.py

---
+++ Day 1/basic_function.py
@@ -0,0 +1,11 @@
+# basic_functions.py
+
+def get_details():
+    print("Enter Student Details")
+    
+def print_details():
+    print("College Admission Completed")
+
+# Function Calls
+get_details()
+print_details()
